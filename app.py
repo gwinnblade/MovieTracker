e5cd43923d7d08aab18e0409f6029e5666895e0c
gwinnblade <lesnov25rus@gmail.com>
2025-10-07 23:16:20 +1000

    added register (testing)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, session, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,6 +9,10 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(300), nullable=False)
     text = db.Column(db.Text, nullable=False)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template('register.html')
 
 @app.route("/")
 @app.route("/index")
